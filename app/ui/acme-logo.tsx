1bcf9f3d90184b80b1b96d3f7ea4a0de0332eeb3
adoydevs <imranadoy@gmail.com>
2025-05-26 00:31:33 +0600

    added

--- app/ui/acme-logo.tsx
+++ app/ui/acme-logo.tsx
@@ -1,5 +1,11 @@
 import { GlobeAltIcon } from '@heroicons/react/24/outline';
-import { lusitana } from "@/app/ui/fonts";
+import { Lusitana } from 'next/font/google';
+const lusitana = Lusitana({
+  weight: '400',
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-lusitana',
+});
 
 
 export default function AcmeLogo() {
